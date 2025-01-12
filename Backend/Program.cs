c258ccd1beacd885a3ceab0fe19325eabee2c870
LitMgwebi <lithim11@gmail.com>
2025-01-12 15:04:34 +0200

    Setup of petty cash

---
+++ Backend/Program.cs
@@ -0,0 +1,155 @@
+#region Global Imports
+
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Data;
+
+#endregion
+
+#region Local Imports
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using System.Text;
+using AutoMapper;
+using Backend;
+
+#endregion
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region Mapping
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+#endregion
+
+#region General
+
+builder.Services.ConfigureIdentity();
+builder.Services.ConfigureJWT(builder.Configuration);
+builder.Services.AddHttpContextAccessor();
+
+#endregion
+
+#region Controllers and SwaggerGen
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(o =>
+    {
+        o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+    });
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorisation",
+        Description = "Bearer Auth with JWT",
+        Type = SecuritySchemeType.Http
+    });
+
+    o.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
+
+#endregion
+
+#region Auth
+
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+});
+//.AddJwtBearer (o =>
+//{
+//    o.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//        ValidAudience = builder.Configuration["Jwt:Audience"],
+//        IssuerSigningKey = new Sy
+//    }
+//});
+builder.Services.AddAuthorization(options =>
+{
+    //options.AddPolicy("StudentAdminPolicy", policy => policy.RequireRole("Student", "Admin"));
+});
+
+
+#endregion
+
+#region Identity
+
+//builder.Services.AddDbContext<PettyCashContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DevelopmentConnection"));
+//    options.EnableSensitiveDataLogging();
+//});
+//builder.Services.AddIdentityCore<User>()
+//    .AddRoles<IdentityRole>()
+//    .AddEntityFrameworkStores<PettyCashContext>()
+//    .AddDefaultTokenProviders();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredUniqueChars = 1;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(20);
+});
+
+#endregion
+
+var app = builder.Build();
+
+#region Web Application
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(options => options
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+//.AllowAnyOrigin()
+//.WithOrigins("http://localhost:8080")
+);
+
+app.UseRouting();
+
+app.UseHttpsRedirection();
+app.UseFileServer();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+#endregion
---
+++ Backend/ServiceExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Backend
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            //IdentityBuilder builder = services.AddIdentityCore<User>(q => q.User.RequireUniqueEmail = true);
+
+            //builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
+            //builder.AddEntityFrameworkStores<PettyCashContext>().AddDefaultTokenProviders();
+        }
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration config)
+        {
+            //var settings = config.GetSection("Jwt");
+            //var key = Environment.GetEnvironmentVariable("JWTKEY");
+
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            //})
+            //    .AddJwtBearer(options =>
+            //    {
+            //        options.TokenValidationParameters = new TokenValidationParameters
+            //        {
+            //            ValidateIssuer = false,
+            //            ValidateLifetime = true,
+            //            ValidateAudience = false,
+            //            ValidateIssuerSigningKey = true,
+            //            ValidIssuer = settings.GetSection("Issuer").Value,
+            //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!))
+            //        };
+            //    });
+        }
+    }
+}
