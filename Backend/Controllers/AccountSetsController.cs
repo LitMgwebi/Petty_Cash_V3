bdc17120fbd0ec5c14e4397fda9ccc97c556d9e6
LitMgwebi <lithim11@gmail.com>
2025-03-15 11:53:29 +0200

    Restructuring accountset controller to exclude command

--- Backend/Controllers/AccountSetsController.cs
+++ Backend/Controllers/AccountSetsController.cs
@@ -4,7 +4,7 @@ namespace Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class AccountSetsController : ControllerBase
     {
         private readonly IAccountSet _account;
@@ -15,19 +15,36 @@ namespace Backend.Controllers
 
         #region GET
 
-        [HttpGet, Route("index")]
-        public async Task<ActionResult> Index(string command)
+        [HttpGet, Route("index_main")]
+        public async Task<ActionResult> IndexMainAccount()
         {
-            ServerResponse<IEnumerable<AccountSet>> response = await _account.GetAllAccountSets(command);
+            ServerResponse<IEnumerable<AccountSet>> response = await _account.GetAllAccountSets(AccountSet.MainAccount);
 
             if (response.Success == false) return BadRequest(response);
             return Ok(response);
         }
 
-        [HttpGet, Route("details")]
-        public async Task<ActionResult> Details(string command, int id)
+        [HttpGet, Route("index_sub")]
+        public async Task<ActionResult> IndexSubAccount()
         {
-            ServerResponse<AccountSet> response = await _account.GetOneAccountSet(command, id);
+            ServerResponse<IEnumerable<AccountSet>> response = await _account.GetAllAccountSets(AccountSet.SubAccount);
+
+            if (response.Success == false) return BadRequest(response);
+            return Ok(response);
+        }
+
+        [HttpGet, Route("details_main")]
+        public async Task<ActionResult> DetailsMainAccount(int id)
+        {
+            ServerResponse<AccountSet> response = await _account.GetOneAccountSet(AccountSet.MainAccount, id);
+            if (response.Success == false) return BadRequest(response);
+            return Ok(response);
+        }
+        
+        [HttpGet, Route("details_sub")]
+        public async Task<ActionResult> DetailsSubAccount(int id)
+        {
+            ServerResponse<AccountSet> response = await _account.GetOneAccountSet(AccountSet.SubAccount, id);
             if (response.Success == false) return BadRequest(response);
             return Ok(response);
         }
@@ -36,10 +53,18 @@ namespace Backend.Controllers
 
         #region POST
 
-        [HttpPost, Route("create")]
-        public async Task<ActionResult<AccountSet>> Create([FromBody] AccountSet account, string command)
+        [HttpPost, Route("create_main")]
+        public async Task<ActionResult<AccountSet>> CreateMainAccount([FromBody] AccountSet account)
         {
-            ServerResponse<AccountSet> response = await _account.CreateAccountSet(account, command);
+            ServerResponse<AccountSet> response = await _account.CreateAccountSet(account, AccountSet.MainAccount);
+            if (response.Success == false) return BadRequest(response);
+            return Ok(response);
+        }
+
+        [HttpPost, Route("create_sub")]
+        public async Task<ActionResult<AccountSet>> CreateSubAccount([FromBody] AccountSet account)
+        {
+            ServerResponse<AccountSet> response = await _account.CreateAccountSet(account, AccountSet.SubAccount);
             if (response.Success == false) return BadRequest(response);
             return Ok(response);
         }
